396b85cb0dc8da4ad023ab1d82e02ca72a738719
Christoph Görn <goern@redhat.com>
2015-11-09 16:01:13 +0100

    add index info

--- cmd/index.go
+++ cmd/index.go
@@ -23,8 +23,12 @@ import (
 	"archive/zip"
 	"bytes"
 	"crypto/tls"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 	jww "github.com/spf13/jwalterweatherman"
@@ -60,23 +64,17 @@ var indexListCmd = &cobra.Command{
 			jww.SetStdoutThreshold(jww.LevelInfo)
 		}
 
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		client := &http.Client{Transport: tr}
-
-		res, err := client.Get("https://github.com/projectatomic/nulecule-library/archive/master.zip")
+		nuleculeLibraryIndexZip, err := getNuleculeLibraryIndexfromGithubAsZIP()
 		if err != nil {
-			jww.FATAL.Printf("Fetching Nulecule Library zip URL failed with error %q\n", err)
+			jww.FATAL.Println(err)
+			return
 		}
 
-		b, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			jww.FATAL.Printf("Failed to read response body with error %q\n", err)
-		}
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 26, 8, 2, '\t', 0)
 
-		r := bytes.NewReader(b)
-		nuleculeLibraryIndexZip, err := zip.NewReader(r, int64(r.Len()))
+		fmt.Fprintln(w, "This is the Nulecule Library Index")
+		fmt.Fprintln(w, "Application Name\tAppID\tVersion")
 
 		// Iterate through the files in the archive
 		for _, item := range nuleculeLibraryIndexZip.File {
@@ -103,12 +101,13 @@ var indexListCmd = &cobra.Command{
 					continue
 				}
 
-				jww.INFO.Printf("%v\n", nuci)
+				fmt.Fprintf(w, "%s\t%s\t%s\n", nuci.Metadata.Name, nuci.AppID, nuci.Metadata.AppVersion)
 			}
+
+			w.Flush()
 
 		}
 
-		defer res.Body.Close()
 	},
 }
 
@@ -119,6 +118,85 @@ var indexInfoCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		InitializeConfig()
 
-		// TODO require one argument
+		if Verbose {
+			jww.SetLogThreshold(jww.LevelTrace)
+			jww.SetStdoutThreshold(jww.LevelInfo)
+		}
+
+		if len(args) < 1 {
+			cmd.Usage()
+			jww.FATAL.Println("APPNAME is required")
+		}
+
+		nuleculeLibraryIndexZip, err := getNuleculeLibraryIndexfromGithubAsZIP()
+		if err != nil {
+			jww.FATAL.Println(err)
+			return
+		}
+
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 26, 8, 2, '\t', 0)
+
+		fmt.Fprintln(w, "This is the Nulecule Library Index")
+		fmt.Fprintln(w, "Application Name\tAppID\tVersion")
+
+		// Iterate through the files in the archive
+		for _, item := range nuleculeLibraryIndexZip.File {
+			if item.FileInfo().IsDir() {
+				continue
+			}
+
+			// if the file is a Nulecule, getit!
+			if item.FileInfo().Name() == "Nulecule" {
+				jww.DEBUG.Printf("Found a Nulecule, size of it's description is %d\n", item.FileInfo().Size())
+
+				rc, err := item.Open()
+				if err != nil {
+					jww.FATAL.Println(err)
+					return
+				}
+				defer rc.Close()
+
+				// get the Nulecules content
+				nuci, parseError := nulecule.Parse(rc)
+
+				if parseError != nil {
+					jww.INFO.Println(parseError, " This may be due to unsupported (by Grasshopper) artifact inheritance.")
+					continue
+				}
+
+				// FIXME this needs to ignore case etc.
+				if (nuci.Metadata.Name == args[0]) || (nuci.AppID == args[0]) {
+					fmt.Fprintf(w, "%s\t%s\t%s\n", nuci.Metadata.Name, nuci.AppID, nuci.Metadata.AppVersion)
+
+				}
+			}
+
+			w.Flush()
+		}
 	},
+}
+
+func getNuleculeLibraryIndexfromGithubAsZIP() (*zip.Reader, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	res, err := client.Get("https://github.com/projectatomic/nulecule-library/archive/master.zip")
+	if err != nil {
+		jww.FATAL.Printf("Fetching Nulecule Library zip URL failed with error %q\n", err)
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		jww.FATAL.Printf("Failed to read response body with error %q\n", err)
+		return nil, err
+	}
+
+	r := bytes.NewReader(b)
+	return zip.NewReader(r, int64(r.Len()))
 }
