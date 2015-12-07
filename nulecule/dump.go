b47178f52f8118975b318e5e4dcf81e26d4ff4ea
Christoph Görn <goern@b4mad.net>
2015-12-07 12:59:06 +0100

    add a dump to string for ContainerApplication

---
+++ nulecule/dump.go
@@ -0,0 +1,34 @@
+/*
+ Copyright 2015 Red Hat, Inc.
+
+ This file is part of Grasshopper.
+
+ Grasshopper is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Grasshopper is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with Grasshopper. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package nulecule
+
+import "github.com/davecgh/go-spew/spew"
+
+//Dump will write a ContainerApplication to STDOUT, if includeExternalNulecules is true
+//it will include all external graph components too.
+func (app ContainerApplication) Dump(includeExternalNulecules bool) string {
+	if includeExternalNulecules {
+		return spew.Sdump(app)
+	} else {
+		// TODO
+	}
+
+	return ""
+}
